1b86a02228f2be2b387b658e26a7f34c10153c04
Xelarednu <dsandr123@gmail.com>
2025-02-13 00:23:31 +0200

    Added correct copying of file names with multiple dots. Now also adds file names beginning with dot.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,8 @@ import java.io.File;
 import java.awt.datatransfer.Clipboard;
 
 public class Main {
+    static int innerDotCounter = 0;
+
     public static void main(String[] args) {
         String dirPath = System.getProperty("user.dir");
 
@@ -15,14 +17,40 @@ public class Main {
 
         if (files != null) {
             StringBuilder fileNamesBuilder = new StringBuilder();
+            int dotCounter;
 
             for (int i = 0; i < files.length; i++) {
+                String fileName = files[i].getName();
+                dotCounter = 0;
 
-                if (files[i].getName().contains("FileList")) {
+                if (fileName.contains("FileList")) {
                     continue;
                 }
 
-                fileNamesBuilder.append(files[i].getName().split("\\.")[0]).append("\n");
+                for (int k = 0; k < fileName.length(); k++) {
+                    if (fileName.charAt(k) == '.') {
+                        dotCounter++;
+                    }
+
+                    if (k == 0 && fileName.charAt(k) == '.') {
+                        fileNamesBuilder.append(fileName);
+                    }
+
+                }
+
+                if (dotCounter == 1) {
+                    fileNamesBuilder.append(fileName.split("\\.")[0]).append("\n");
+                } else if (dotCounter > 1) {
+                    for (int j = 0; j < fileName.length(); j++) {
+                        if (innerDotCounter(fileName) == dotCounter) {
+                            fileNamesBuilder.append(fileName.split("\\.")[j]).append("\n");
+                            innerDotCounter = 0;
+                            break;
+                        }
+
+                        fileNamesBuilder.append(fileName.split("\\.")[j]).append(".");
+                    }
+                }
             }
 
             String fileNames = fileNamesBuilder.toString();
@@ -31,7 +59,16 @@ public class Main {
             StringSelection stringSelection = new StringSelection(fileNames);
             Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
             clipboard.setContents(stringSelection, null);
-
         }
+    }
+    
+    public static int innerDotCounter(String fileName) {
+        for (int i = 0; i < fileName.length(); i++) {
+            if (fileName.charAt(i) == '.') {
+                innerDotCounter++;
+                break;
+            }
+        }
+        return innerDotCounter;
     }
 }
